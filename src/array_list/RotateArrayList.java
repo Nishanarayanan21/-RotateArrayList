fefcea30767db0e9bf8da165031dc9d64f0729ab
nishanthini <nishanthini2176@gmail.com>
2026-06-09 14:20:54 +0530

    Implemented RotateArrayList program

--- src/array_list/RotateArrayList.java
+++ src/array_list/RotateArrayList.java
@@ -1,18 +1,16 @@
 package array_list;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class RotateArrayList
-{
-    public ArrayList<Integer> rotateRight(ArrayList<Integer> list, int k)
-    {
+public class RotateArrayList {
+    public ArrayList<Integer> rotateRight(ArrayList<Integer> list, int k) {
         Collections.rotate(list, k);
         return list;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of elements: ");
@@ -22,8 +20,7 @@ public class RotateArrayList
 
         System.out.println("Enter elements:");
 
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             list.add(sc.nextInt());
         }
 
@@ -34,5 +31,8 @@ public class RotateArrayList
 
         System.out.println("Rotated ArrayList:");
         System.out.println(obj.rotateRight(list, k));
+
+        sc.close();
     }
 }
+
